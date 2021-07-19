de70fa44fe5c01ff125d611866291e74591adf20
harshitsinghai77 <harshitsinghai77@gmail.com>
2021-07-19 22:09:46 +0530

    fix TooManyConnection, add min_size, max_size connection to postgres string

--- noiist/config/database.py
+++ noiist/config/database.py
@@ -1,6 +1,6 @@
 import os
 
-import databases
+from databases.backends.postgres import PostgresBackend
 import sqlalchemy
 
 DB_NAME = os.getenv("DB_NAME", "habitt")
@@ -9,13 +9,13 @@ DB_PASSWORD = os.getenv("DB_PASSWORD", "habitt")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5436")
 DB_MAX_SIZE = 10
-
 environment = os.getenv("ENV", "development")
+
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 if environment == "development":
     DATABASE_URL = "postgresql://noisli:password@localhost:5432/noisli"
 
-database = databases.Database(DATABASE_URL, max_size=DB_MAX_SIZE)
+database = PostgresBackend(DATABASE_URL, min_size=1, max_size=DB_MAX_SIZE)
 metadata = sqlalchemy.MetaData()
 
 engine = sqlalchemy.create_engine(DATABASE_URL)
