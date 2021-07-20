40393cbc99cb0c8261d3cc0604cdea64874f1fd6
harshitsinghai77 <harshitsinghai77@gmail.com>
2021-07-20 23:22:34 +0530

    test init

--- tests/test_noisli.py
+++ tests/test_noisli.py
@@ -1,16 +1,5 @@
-from fastapi.testclient import TestClient
-
 from noiist import __version__
-from main import app
-
-client = TestClient(app)
 
 
 def test_version():
     assert __version__ == "0.1.0"
-
-
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.text == "App succesfully running."
