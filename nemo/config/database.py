f33883a03b1bc285e59be87992d46deb65194fe0
harshitsinghai77 <harshitsinghai77@gmail.com>
2021-08-08 22:19:20 +0530

    Add dev env postgres support

--- nemo/config/database.py
+++ nemo/config/database.py
@@ -9,8 +9,11 @@ DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_MAX_SIZE = 10
+environment = os.getenv("ENV", "development")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+if environment == "development":
+    DATABASE_URL = "postgresql://nemo:password@localhost:5432/nemo"
 
 database = databases.Database(DATABASE_URL + f"?min_size=1&max_size={DB_MAX_SIZE}")
 metadata = sqlalchemy.MetaData()
