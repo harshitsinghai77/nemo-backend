0384f7ac4edb9f1adce722986d89398184456312
harshitsinghai77 <harshitsinghai77@gmail.com>
2021-08-08 22:17:32 +0530

    Add support for apache benchmark

--- Makefile
+++ Makefile
@@ -21,3 +21,6 @@ format:
 	
 lint:
 	pylint nemo
+
+apache_benchmark:
+	ab -c 100 -n 10000 http://127.0.0.1:8000/
--- nemo/config/database.py
+++ nemo/config/database.py
@@ -7,13 +7,10 @@ DB_NAME = os.getenv("DB_NAME", "nemo")
 DB_USER = os.getenv("DB_USER", "nemo")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
 DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_PORT = os.getenv("DB_PORT", "5436")
+DB_PORT = os.getenv("DB_PORT", "5432")
 DB_MAX_SIZE = 10
 
-environment = os.getenv("ENV", "development")
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-if environment == "development":
-    DATABASE_URL = "postgresql://nemo:password@localhost:5432/nemo"
 
 database = databases.Database(DATABASE_URL + f"?min_size=1&max_size={DB_MAX_SIZE}")
 metadata = sqlalchemy.MetaData()
