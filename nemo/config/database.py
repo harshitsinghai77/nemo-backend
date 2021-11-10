a22eda80e3653081af016a403861ddbf5e27c21f
harshitsinghai77 <harshitsinghai77@gmail.com>
2021-11-10 07:35:58 +0530

    change database to heroku deployment, remove repeat_after to update cache

--- nemo/config/database.py
+++ nemo/config/database.py
@@ -1,22 +1,21 @@
 import os
-from urllib.parse import quote
+
+# from urllib.parse import quote
 
 import databases
 import sqlalchemy
 
-DB_NAME = os.getenv("QOVERY_POSTGRESQL_Z794A99A3_DEFAULT_DATABASE_NAME", "nemo")
-DB_USER = os.getenv("QOVERY_POSTGRESQL_Z794A99A3_LOGIN", "nemo")
-DB_PASSWORD = os.getenv("QOVERY_POSTGRESQL_Z794A99A3_PASSWORD", "password")
-DB_HOST = os.getenv("QOVERY_POSTGRESQL_Z794A99A3_HOST", "localhost")
-DB_PORT = os.getenv("QOVERY_POSTGRESQL_Z794A99A3_PORT", "5432")
+DB_NAME = os.getenv("DB_NAME", "nemo")
+DB_USER = os.getenv("DB_USER", "nemo")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "password")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "5432")
 DB_MAX_SIZE = 10
 
 environment = os.getenv("ENV", "development")
 
-ENCODED_PASSWORD = quote(DB_PASSWORD)
-DATABASE_URL = (
-    f"postgresql://{DB_USER}:{ENCODED_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+# ENCODED_PASSWORD = quote(DB_PASSWORD)
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 if environment == "development":
     DATABASE_URL = "postgresql://nemo:password@localhost:5432/nemo"
 
