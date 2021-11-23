6f4fdfde103e4e715f97270b639ca34902d7ad5e
harshitsinghai77 <harshitsinghai77@gmail.com>
2021-11-24 04:25:11 +0530

    remove cors origin

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ app = FastAPI(title=settings.APP_NAME)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
